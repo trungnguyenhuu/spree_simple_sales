1669467cffbf8fc7ac719b64eec4abcf94da2380
trungnguyenhuu <trungnguyenhuu262@gmail.com>
2016-01-19 10:34:31 +0700

    Update add sale price to product form

--- app/overrides/add_sale_price_to_product_edit.rb
+++ app/overrides/add_sale_price_to_product_edit.rb
@@ -2,21 +2,28 @@ Deface::Override.new(:virtual_path => 'spree/admin/products/_form',
   :name => 'add_sale_price_to_product_edit',
   :insert_after => "erb[loud]:contains('text_field :price')",
   :text => "
-    <%= f.field_container :sale_price do %>
-      <%= f.label :sale_price, raw(Spree.t(:sale_price) ) %>
-      <%= f.text_field :sale_price, :value =>
-        number_to_currency(@product.sale_price, :unit => '') %>
-      <%= f.error_message_on :sale_price %>
-    <% end %>
+      <div data-hook='admin_product_sale_price'>
+        <%= f.field_container :sale_price, class: ['form-group'] do %>
+          <%= f.label :sale_price, Spree.t(:sale_price)%>
+          <%= f.text_field :sale_price, value: number_to_currency(@product.sale_price, unit: ''), class: 'form-control', disabled: (cannot? :update, Spree::Price) %>
+          <%= f.error_message_on :sale_price %>
+        <% end %>
+      </div>
 
-    <%= f.field_container :sale_start_date do %>
-      <%= f.label :sale_start_date, raw(Spree.t(:sale_start_date)) %>
-      <%= f.text_field :sale_start_date, value: @product.sale_start_date, class: 'datepicker' %>
-    <% end %>
+      <div data-hook='admin_product_sale_start_date'>
+        <%= f.field_container :sale_start_date, class: ['form-group'] do %>
+          <%= f.label :sale_start_date, Spree.t(:sale_start_date)%>
+          <%= f.text_field :sale_start_date, value: datepicker_field_value(@product.sale_start_date), class: 'datepicker form-control' %>
+          <%= f.error_message_on :sale_start_date %>
+        <% end %>
+      </div>
 
-    <%= f.field_container :sale_end_date do %>
-      <%= f.label :sale_end_date, raw(Spree.t(:sale_end_date)) %>
-      <%= f.label :sale_end_date, raw(Spree.t(:sale_end_date)) %>
-      <%= f.text_field :sale_end_date, value: @product.sale_end_date, class: 'datepicker' %>
-    <% end %>
+      <div data-hook='admin_product_sale_end_date'>
+        <%= f.field_container :sale_end_date, class: ['form-group'] do %>
+          <%= f.label :sale_end_date, Spree.t(:sale_end_date)%>
+          <%= f.text_field :sale_end_date, value: datepicker_field_value(@product.sale_end_date), class: 'datepicker form-control' %>
+          <%= f.error_message_on :sale_end_date %>
+        <% end %>
+      </div>
+
   ")
